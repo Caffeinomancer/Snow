5a01123e8da2421455e5ba02ed7522bb017c5f57
Dan Covert <covertcoding@gmail.com>
2022-10-14 03:07:37 -0400

    updated movement

--- ProjectSnow/Assets/Scripts/PlayerMovement.cs
+++ ProjectSnow/Assets/Scripts/PlayerMovement.cs
@@ -5,38 +5,52 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
-    public float moveSpeed;
+    private float moveSpeed;
+    public float walkSpeed;
+    public float sprintSpeed;
     public float groundDrag;
+    
+    [Header("Jumping")]
     public float jumpForce;
     public float jumpCooldown;
     public float airMultiplier;
+    private bool canJump;
 
 
     [Header("Keybinds")]
     public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
 
     [Header("Ground Check")]
     public float playerHeight;
     public LayerMask whatIsGround;
     private bool isGrounded;
 
-
-    
+    [Header("Slope handling")]
+    public float maxSlopeAngle;
+    private RaycastHit slopeHit;
+    private bool exitingSlope;
 
     private float horizontalInput;
     private float verticalInput;
 
-    private bool canJump;
-
-    
+    //Player dimensions
+    private float playerHalfHeight = 0.5f;
+    private float playerHalfHeightOffSet = 0.3f;
 
     public Transform orientation;
-
 
     private Vector3 movementDirection;
 
     private Rigidbody rb;
 
+    public MovementState state;
+    public enum MovementState
+    {
+        walking,
+        sprinting,
+        air
+    }
 
     void Start()
     {
@@ -48,10 +62,11 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * playerHalfHeight + playerHalfHeightOffSet, whatIsGround);
 
         UpdateInput();
         SpeedControl();
+        StateHandler();
 
         if(isGrounded)
         {
@@ -84,9 +99,41 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    private void StateHandler()
+    {
+        //Sprinting 
+        if(isGrounded && Input.GetKey(sprintKey))
+        {
+            state = MovementState.sprinting;
+            moveSpeed = sprintSpeed;
+        }
+
+        else if(isGrounded)
+        {
+            state = MovementState.walking;
+            moveSpeed = walkSpeed;
+        }
+
+        else
+        {
+            state = MovementState.air;
+        }
+    }
+
     private void MovePlayer()
     {
         movementDirection = (orientation.forward * verticalInput) + (orientation.right * horizontalInput);
+
+        //Slope
+        if(OnSlope() && !exitingSlope)
+        {
+            rb.AddForce(GetSlopeMoveDirection() * moveSpeed * 20.0f, ForceMode.Force);
+
+            if(rb.velocity.y > 0)//If moving upwards add down velocity to keep on slope
+            {
+                rb.AddForce(Vector3.down * 80.0f, ForceMode.Force);
+            }
+        }
 
         //Grounded
         if(isGrounded)
@@ -99,22 +146,40 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.AddForce(movementDirection.normalized * moveSpeed * 10.0f * airMultiplier, ForceMode.Force);
         }
+
+        //Turn Gravity off if on a slope
+        rb.useGravity = !OnSlope();
     }
 
     private void SpeedControl()
     {
-        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        //Limit speed on slope
 
-        //Limit velocity
-        if(flatVel.magnitude > moveSpeed)
+        if(OnSlope() && !exitingSlope)
         {
-            Vector3 limitedVel = flatVel.normalized * moveSpeed; //calculate what new max velocity would be
-            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z); //apply velocity
+            if(rb.velocity.magnitude > moveSpeed)
+            {
+                rb.velocity = rb.velocity.normalized * moveSpeed;
+            }
+        }
+
+        else
+        {
+            Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+            //Limit velocity
+            if (flatVel.magnitude > moveSpeed)
+            {
+                Vector3 limitedVel = flatVel.normalized * moveSpeed; //calculate what new max velocity would be
+                rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z); //apply velocity
+            }
         }
     }
 
     private void Jump()
     {
+        exitingSlope = true;
+
         //Reset jump velocity to zero so we only always jump the same height
         rb.velocity = new Vector3(rb.velocity.x, 0.0f, rb.velocity.z);
 
@@ -124,5 +189,25 @@ public class PlayerMovement : MonoBehaviour
     private void ResetJump()
     {
         canJump = true;
+
+        exitingSlope = false;
     }
+
+    private bool OnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * playerHalfHeight + playerHalfHeightOffSet))
+        {
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            return angle < maxSlopeAngle && angle != 0;
+        }
+
+        return false;
+    }
+
+    private Vector3 GetSlopeMoveDirection()
+    {
+        return Vector3.ProjectOnPlane(movementDirection, slopeHit.normal).normalized;
+    }    
+
+  
 }
